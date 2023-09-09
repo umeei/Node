45a8ed9861c066b2e1284828e14a1bf995707a9e
umair <umairjutt2025@gmail.com>
2023-09-09 18:14:03 +0500

    Again Commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const port= 2000;
+const port=process.env.PORT || 3000;
 const mongoose = require('mongoose');
 app.use(express.json())
 
@@ -14,7 +14,7 @@ app.use("/api/teacher",Teacherapi)
 
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/Sdata').then((res)=>{
+mongoose.connect('mongodb+srv://umairjutt2025:umairjutt2025@umair-cluster.oducycs.mongodb.net/Sdata?retryWrites=true&w=majority').then((res)=>{
     console.log("Database is connected!")
 }).catch((err)=>{
     console.log(err.message)
